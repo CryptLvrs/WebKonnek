e9cff3840b2f944c08b1325781694e9ddcd71d27
CryptLvrs <alatabaptiste@gmail.com>
2026-08-09 00:28:21 +0200

    Finalisation of 'loadavg'

--- sys_info.c
+++ sys_info.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *fptr;
-    fptr = fopen("/proc/loadavg", "r");
+    FILE *fptr = fopen("/proc/loadavg", "r");
     if (fptr == 0) {
         printf("Error : Unable to open file called 'loadavg'.\n");
         return 1;
@@ -11,24 +10,23 @@ int main() {
 
     printf("Success : 'loadavg' file is open.\n");
 
-    char fline[5];
-
+    char fline[15];
     if (fgets(fline, sizeof(fline), fptr) == NULL) {
         printf("Error : Failed to read from file.\n");
         fclose(fptr);
         return 1;
     }
 
-    char *endptr;
-    float loadst = strtof(fline, &endptr);
-
-    if (endptr == NULL) {
+    float load1min, load5min, load15min;
+    if (sscanf(fline, "%f %f %f", &load1min, &load5min, load15min) == -1) {
         printf("Error : There isn't any float number for one min load average in file 'loadavg'.\n");
         fclose(fptr);
         return 1;
     }
 
-    printf("%.2f\n", loadst);
+    printf("load1 : %.2f\n", load1min);
+    printf("load5 : %.2f\n", load5min);
+    printf("load 15 : %.2f\n", load15min);
 
     return 0;
 }
