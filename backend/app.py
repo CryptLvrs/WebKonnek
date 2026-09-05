bed934338054b7869b5540f5ac8e171b59904627
CryptLvrs <alatabaptiste@gmail.com>
2026-09-05 08:11:46 +0200

    remove debug flask

--- backend/app.py
+++ backend/app.py
@@ -63,7 +63,7 @@ def get_stats():
     return jsonify(system_data)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True) 
+    app.run(host="0.0.0.0", port=5000) 
 
 # COMMANDS SECTIONS FOR DEV
 # source .venv/bin/activate
